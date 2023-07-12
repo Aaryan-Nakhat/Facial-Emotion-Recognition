ea8d84813c897909d97469f6e8afc8caa095c120
Aaryan Nakhat <108934499+Aaryan-Nakhat@users.noreply.github.com>
2023-07-12 20:26:51 +0530

    Update video_inference.py

--- video_inference.py
+++ video_inference.py
@@ -1,5 +1,4 @@
 # Imports
-
 import numpy as np
 import cv2
 from PIL import Image
@@ -10,22 +9,14 @@ from tensorflow.keras.models import load_model
 from typing import Optional
 import argparse
 
-
-
 # Defining the Command Line Arguments
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_to_use", type=int, help="which model to use (pass an integer among [0, 1, 2, 3, 4]",  required=True)
 parser.add_argument("--enable_real_time_video_inferencing", help="whether to enable real-time video inferencing (type: bool)", required=True)
 parser.add_argument("--prerecorded_video_path", help="path to the pre-recorded video")
 
-
-
 # Helper Variables
-
 class_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
-
-
 available_models = ['./models/Emotion_Recognition_CNN.h5', 
                     './Emotion_Recognition_CNN_with_Data_Aug.h5', 
                     'Aaryan333/vit-base-finetuned-fer2013',
@@ -33,30 +24,24 @@ available_models = ['./models/Emotion_Recognition_CNN.h5',
                     'Aaryan333/convnextv2-tiny-384-finetuned-fer2013'
                    ]
 
-
-
 # Video FER Inferencing Function
-
 def do_video_inferencing(model_to_use: int, enable_real_time_video_inferencing: Optional[bool] = False, 
                prerecorded_video_path: Optional[str] = None):
         
     if model_to_use in range(5):
         face_detector = cv2.CascadeClassifier("haarcascades_models/haarcascade_frontalface_default.xml")
         if model_to_use < 2:
-            emotion_recognition_model = load_model(available_models[model_to_use])
-            
+            emotion_recognition_model = load_model(available_models[model_to_use])   
         else:
             feature_extractor = AutoFeatureExtractor.from_pretrained(available_models[model_to_use])
             model = AutoModelForImageClassification.from_pretrained(available_models[model_to_use])
-            pipe = pipeline("image-classification", model=model, feature_extractor=feature_extractor)
-            
+            pipe = pipeline("image-classification", model=model, feature_extractor=feature_extractor)   
             
         if enable_real_time_video_inferencing:
             cap = cv2.VideoCapture(0)
         else:
             if prerecorded_video_path == None:
-                raise ValueError("Please provide a pre-recorded video path!")
-                
+                raise ValueError("Please provide a pre-recorded video path!")  
             video_path = fr"{prerecorded_video_path}"
             cap = cv2.VideoCapture(video_path)
 
@@ -64,7 +49,6 @@ def do_video_inferencing(model_to_use: int, enable_real_time_video_inferencing: 
             ret, frame = cap.read() # ret is a boolean variable that returns true if the frame is available. frame is an image array vector captured based on the default frames per second    
             if not ret:
                 break
-
             gray_scale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_detector.detectMultiScale(image=gray_scale_frame, scaleFactor=1.3, minNeighbors=5)
 
@@ -93,17 +77,13 @@ def do_video_inferencing(model_to_use: int, enable_real_time_video_inferencing: 
             k = cv2.waitKey(30) & 0xff
             if k == 27:      #Press Esc to stop the video
                 break
-
         cap.release()
-        cv2.destroyAllWindows()       
-            
+        cv2.destroyAllWindows()               
     else:
         raise ValueError("model_to_use must be an integer among [0, 1, 2, 3, 4]")
 
 
-
 # Getting the Command Line Arguments
-
 args = parser.parse_args().__dict__ 
 a = args['model_to_use']
 boolean_checker = isinstance(args['enable_real_time_video_inferencing'], bool)
@@ -120,9 +100,6 @@ if len(args)==3:
 else:
 	c = None
 
-
-
-
 # Calling the function
-
-do_video_inferencing(model_to_use=a, enable_real_time_video_inferencing=b, prerecorded_video_path=c)
+if __name__ == "__main__":
+	do_video_inferencing(model_to_use=a, enable_real_time_video_inferencing=b, prerecorded_video_path=c)
